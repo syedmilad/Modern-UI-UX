455c405ed4b735e8a8a55b102e3e31a9d2074ec9
milad <syedmilad99@gmail.com>
2024-07-06 10:47:54 -0700

    updated work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <div className="bg-primary w-full overflow-hidden">
+          <h1>Hello World!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
