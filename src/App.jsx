fd622e1c5694697e1ca65234fff702ee9540436a
milad <syedmilad99@gmail.como>
2024-07-08 22:50:08 +0500

    updated work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import { Billing,Testimonials, Business, CardDeal, Clients, CTA, Footer, Stats } from "./components";
-import style from "./style"
+import {
+  Billing,
+  Testimonials,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Stats,
+  Hero,
+  Navbar,
+} from "./components";
+import style from "./style";
 
 const App = () => {
-  return <div className="bg-primary w-full overflow-hidden">
-    <div className={`${style.paddingX} ${style.flexCenter}`}>
-        <div className={`${style.boxWidth}`}> 
-          Navbar
-        </div>
-    </div>
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}><Navbar/></div>
+      </div>
 
-    <div className={`${style.paddingX} ${style.flexStart}`}>
-        <div className={`${style.boxWidth}`}> 
-        Hero
-        </div>
-    </div>
+      <div className={`${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Hero/>
+          </div>
+      </div>
 
-    <div className={`${style.paddingX} ${style.flexStart}`}>
-        <div className={`${style.paddingX} ${style.boxWidth}`}> 
+      <div className={`${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.paddingX} ${style.boxWidth}`}>
           <Stats></Stats>
           <Business></Business>
           <Billing></Billing>
@@ -26,10 +36,9 @@ const App = () => {
           <CTA></CTA>
           <Footer></Footer>
         </div>
+      </div>
     </div>
-
-
-  </div>;
+  );
 };
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section className={`flex md:flex-row flex-col xl:px-0 sm:px-16 p-6`}>
+      <div>asmdflaisd</div>
+      <div>asmdflaisd</div>
+    </section>
   )
 }
