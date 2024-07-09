32575c2c3f0d10ad7e8609c7333ec4f17da7c6d3
milad <syedmilad99@gmail.como>
2024-07-09 23:51:23 +0500

    updated work

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { arrowUp } from '../assets'
 
 export default function GetStarted() {
   return (
-    <div>GetStarted</div>
+    <div className='flex justify-center items-center w-[134px] h-[134px] rounded-full bg-blue-gradient p-[2px] cursor-pointer'>
+
+      <div className='flex flex-col items-center justify-center rounded-full text-white bg-primary w-full h-full'>
+        
+        <div className='font-poppins flex items-center justify-center gap-1 font-medium text-[18px] leading=[23px]'>
+          <span className='text-gradient'>Get</span>
+          <img src={arrowUp} alt="arrowUp" className='w-[23px] h-[23px] object-contain'/>
+        </div>
+
+        <div className='font-poppins gap-1 font-medium text-[18px] leading=[23px]'>
+          <span className='text-gradient'>Started</span>
+        </div>
+
+      </div>  
+    </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
-
+import { discount } from "../assets"
+import GetStarted from "./GetStarted"
 export default function Hero() {
   return (
-    <section className={`flex md:flex-row flex-col xl:px-0 sm:px-16 p-6`}>
-      <div>asmdflaisd</div>
-      <div>asmdflaisd</div>
+    <section className=" flex md:flex-row flex-col sm:py-12 py-6">
+      <div className="flex-1 flex justify-center items-start flex-col xl:px-0 md:px-16 px-6 ">
+
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-xl mb-2">
+           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className="ml-2 font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">
+          <span className="text-white">20%</span> Discount For {" "}
+          <span className="text-white">1 Month</span> Account
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+            <h1 className="flex-1 font-poppins font-semibold ss:text[77px] text-[52px] text-white ss:leading-[100px] leading-[77px]">
+              The Next <br className="sm:block hidden" />{" "}
+              <span className="text-gradient">Generation <br /></span>{" "} 
+              <span>Payment Method.</span>
+            </h1>
+
+            <div className="ss:flex hidden md:mr-4">
+              <GetStarted/>
+            </div>
+        </div>
+      </div>
     </section>
   )
 }
